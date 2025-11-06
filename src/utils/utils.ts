3166bef1f9cbd923b60b7925c0ab06473d0dec49
SyahrulBhudiF <syahrul4w@gmail.com>
2025-11-07 02:14:59 +0700

    feat(env): adding generic env utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,5 +2,11 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
+}
+
+export function requireEnv<T>(name: string): T {
+  const value = process.env[name];
+  if (!value) throw new Error(`Missing environment variable: ${name}`);
+  return value as T;
 }
