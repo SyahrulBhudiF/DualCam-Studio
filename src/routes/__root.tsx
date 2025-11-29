be37fcb5bec6235f6696fe9d9e7df0416a4b1e82
SyahrulBhudiF <syahrul4w@gmail.com>
2025-11-30 01:15:19 +0700

    chore: seo details

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -37,9 +37,8 @@ export const Route = createRootRouteWithContext<{
         content: "width=device-width, initial-scale=1",
       },
       ...seo({
-        title:
-          "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
-        description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
+        title: "DualCam Studio",
+        description: `DualCam Studio is a general‑purpose, dataset‑oriented platform for dual‑camera microexpression recording and questionnaire management, suitable both for research and for more traditional online exam and survey use cases.`,
       }),
     ],
     links: [
